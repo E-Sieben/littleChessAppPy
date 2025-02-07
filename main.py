e7cbea0616265959a145b17b4e92dbbf056dda1b
Eric Sieben <business@esieben.net>
2025-02-07 12:34:59 +0000

    Proper Display

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import chess
 from contestants import white, black
 from tqdm import tqdm
 
-total_games = 10000
+total_games = 100
 white_wins = 0
 black_wins = 0
 ties = 0
@@ -18,14 +18,14 @@ for i in progress_bar:
     board = chess.Board()
     turns = 0
     while not board.is_game_over() and turns < 1000:
-        # Player 1 move
+        # * Player 1 move
         move = white.move(board)
         if move is not None:
             board.push_uci(move)
         turns += 1
         if board.is_game_over():
             break
-        # Player 2 move
+        # * Player 2 move
         move = black.move(board)
         if move is not None:
             board.push_uci(move)
@@ -47,13 +47,14 @@ for i in progress_bar:
 
 print("----------------------------------------------")
 print(
-    f"♚  White won {(white_wins/total_games)*100}% of Games, that's {white_wins} times")
+    f"♚  White won {int(round((white_wins/total_games)*100, 1))}% of Games, that's {white_wins} times")
 print(
-    f"♔  Black won {(black_wins/total_games)*100}% of Games, that's {black_wins} times")
-print(f"⚔️  Game tied {(ties/total_games)*100}% of Games, that's {ties} times")
+    f"♔  Black won {int(round((black_wins/total_games)*100, 1))}% of Games, that's {black_wins} times")
+print(
+    f"⚔️  Game tied {int(round((ties/total_games)*100, 1))}% of Games, that's {ties} times")
 print("-----------------------------------------------")
 if white_wins == black_wins:
-    print("=> ⚔️  It's a Tie!")
+    print("=> No one Won! ⚔️  It's a Tie!")
 else:
     print(
         f"=> The Winner is {"♚  White!" if white_wins > black_wins else "♔  Black!"}"
