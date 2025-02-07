1ce6bac1f6ce5785ac507bc5712d82a2872da37c
Eric Sieben <business@esieben.net>
2025-02-07 12:49:47 +0000

    Type Safety

--- main.py
+++ main.py
@@ -5,43 +5,44 @@ pip install chess tqdm
 import chess
 from contestants import white, black
 from tqdm import tqdm
+
 #! Settings
-total_games = 100
-white_wins = 0
-black_wins = 0
-ties = 0
-last_winner = None
+total_games: int = 100
+white_wins: int = 0
+black_wins: int = 0
+ties: int = 0
+last_winner: str = None
 
 progress_bar = tqdm(range(total_games), desc="Games")
 for i in progress_bar:
     #! Init/Reset Game
     progress_bar.set_description(f"Games (Last winner: {last_winner})")
-    board = chess.Board()
-    turns = 0
+    board: chess.Board = chess.Board()
+    turns: int = 0
     #! Start Game
     while not board.is_game_over() and turns < 1000:
         # * Player 1 move
-        move = white.move(board)
+        move: str = white.move(board)
         if move is not None:
             board.push_uci(move)
         turns += 1
         if board.is_game_over():
             break
         # * Player 2 move
-        move = black.move(board)
+        move: str = black.move(board)
         if move is not None:
             board.push_uci(move)
         turns += 1
     #! Evaluate game outcome
     outcome = board.outcome()
     if outcome is not None:
-        winner = outcome.winner
+        winner: bool = outcome.winner
         if winner is True:
             white_wins += 1
-            last_winner = "White"
+            last_winner: str = "White"
         elif winner is False:
             black_wins += 1
-            last_winner = "Black"
+            last_winner: str = "Black"
     else:
         ties += 1
 
