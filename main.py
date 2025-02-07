b22d2c0a2c28d042e21060766f407703810937ce
Eric Sieben <business@esieben.net>
2025-02-07 12:22:42 +0000

    Added better Game Logic and Output

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
 import chess
-from bots import BogoBot
+from contestants import white, black
+from tqdm import tqdm
 
-board = chess.Board()
-turns = 0
-whiteWins = 0
-blackWins = 0
+total_games = 1000
+white_wins = 0
+black_wins = 0
 ties = 0
-for i in range(1000):
-    # ? Game
-    while not board.is_stalemate() and not board.is_insufficient_material() and turns < 1000 and len(list(board.legal_moves)) > 0:
-        move = BogoBot.move(board)
-        board.push_uci(move)
-        if not board.is_stalemate() and not board.is_insufficient_material() and turns < 1000 and len(list(board.legal_moves)) > 0:
-            move = BogoBot.move(board)
+last_winner = None
+
+progress_bar = tqdm(range(total_games), desc="Games")
+
+for i in progress_bar:
+    board = chess.Board()
+    turns = 0
+    while not board.is_game_over() and turns < 1000:
+        # Player 1 move
+        move = white.move(board)
+        if move is not None:
             board.push_uci(move)
         turns += 1
-    # ? Eval
-    try:
-        outcome = board.outcome().winner
-        if outcome == True:
-            whiteWins = + 1
-        elif outcome == False:
-            blackWins += 1
-        else:
-            ties += 1
-    except:
-        outcome = None
-    board.clear()
+        if board.is_game_over():
+            break
+        # Player 2 move
+        move = black.move(board)
+        if move is not None:
+            board.push_uci(move)
+        turns += 1
+    # Evaluate game outcome
+    outcome = board.outcome()
+    if outcome is not None:
+        winner = outcome.winner
+        if winner is True:
+            white_wins += 1
+            last_winner = "White"
+        elif winner is False:
+            black_wins += 1
+            last_winner = "Black"
+    else:
+        ties += 1
+    # Update progress bar description
+    progress_bar.set_description(f"Games (Last winner: {last_winner})")
+
+print("----------------------------------------------")
 print(
-    f"White won {whiteWins} times \nBlack won {blackWins} times \nThe Game tied {ties} times")
+    f"♚  White won {(white_wins/total_games)*100}% of Games, that's {white_wins} times")
+print(
+    f"♔  Black won {(black_wins/total_games)*100}% of Games, that's {black_wins} times")
+print(f"⚔️  Game tied {(ties/total_games)*100}% of Games, that's {ties} times")
+print("-----------------------------------------------")
+if white_wins == black_wins:
+    print("=> ⚔️  It's a Tie!")
+else:
+    print(
+        f"=> The Winner is {"♚  White!" if white_wins > black_wins else "♔  Black!"}"
+    )
